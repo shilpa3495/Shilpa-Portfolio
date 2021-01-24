9044f45a37dc7c355cbb1d014f56651a29454f4d
shilpa3495 <shilpaaggarwal3495@gmail.com>
2021-01-24 14:43:35 +0530

    remove bootstrap

--- js/main.js
+++
@@ -1,5 +0,0 @@
-var myCollapseEl = document.getElementById('navbarToggleExternalContent')
-
-myCollapseEl.addEventListener('shown.bs.collapse', function (event) {
-  // Action to execute once the collapsible area is expanded
-})
